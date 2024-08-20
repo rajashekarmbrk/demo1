e29f0d670348a83ea156c5946dd424ed1c2e49d8
rajashekarmbrk <169077838+rajashekarmbrk@users.noreply.github.com>
2024-08-20 10:22:34 +0530

    Add files via upload

---
+++ jsofun.js
@@ -0,0 +1,12 @@
+let person ={
+    name:"john",
+    age:"30",
+    occupation:"Developer"
+
+    
+};
+function greet() {
+    console.log("Hello, my name is " + person.name); 
+}
+
+greet()
---
+++ type.js
@@ -0,0 +1,2 @@
+let person = {}
+console.log(typeof person);
